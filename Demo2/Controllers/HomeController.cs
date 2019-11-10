d515a0ec557691d7d6f2618794208cdca99e7395
kingkrows <56763132+kingkrows@users.noreply.github.com>
2019-11-10 14:35:23 +0600

    without database

---
+++ Demo2/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Demo2.Models;
+using Demo2.ViewModels;
+
+namespace Demo2.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            //PageTitle = "Employee Details";
+            return View(model);
+        }
+
+        
+        public ViewResult Details(int? id)
+        {
+
+            HomeDetailsViewModels homeDetailsViewModels = new HomeDetailsViewModels()
+            {
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details"
+            };
+           // Employee model = _employeeRepository.GetEmployee(1);
+            // ViewData["Employee"] = model;
+            //ViewData["PageTitle"] = "Employee Details";
+
+           // ViewBag.Employee = model;
+
+
+          //  ViewBag.PageTitle = "Employee Details";
+            return View(homeDetailsViewModels);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+
+            return View();
+
+          
+        }
+    }
+}
---
+++ Demo2/ViewModels/HomeDetailsViewModels.cs
@@ -0,0 +1,15 @@
+﻿using Demo2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo2.ViewModels
+{
+    public class HomeDetailsViewModels
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+
+    }
+}
